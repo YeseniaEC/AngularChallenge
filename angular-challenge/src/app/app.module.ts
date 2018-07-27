329bc3aa530752a810ff0ef9098beb695eb60c46
Yesenia <eliyessie@gmail.com>
2018-07-26 17:44:14 -0700

    deleted extra images and unused component

--- angular-challenge/src/app/app.module.ts
+++ angular-challenge/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponent } from './home/home.component';
-import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
     HomeComponent,
-    MovieDetailsComponent,
   ],
   imports: [
     BrowserModule,
